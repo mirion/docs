2343cd8664d295cfd55ff7aa28c5cb94ab50f794
mirion <mirionc@gmail.com>
2014-09-06 00:27:12 +0300

    Dash docset generator - fix the code blocks generation

--- vendor/jsdoc/app/plugins/smartdown/smartdown.js
+++ vendor/jsdoc/app/plugins/smartdown/smartdown.js
@@ -10,18 +10,25 @@ var Smartdown = {
       two
         4, and so on.
     */
-    
+
     var initial = text.match(/\n([ \t]+)/);
     if (initial) {
       text = text.replace(new RegExp("^" + initial[1], 'mg'), "");
     }
-    
+
     if (text.match(/\n/)) text = new Showdown.converter().makeHtml(text);
 
     text = text.replace(/<code>(#([^:\s]+)?)?:?\s*([^\0]+?)<\/code>/g, function(match, whitespace, lang, code) {
       lang = lang || "js";
       code = code.replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/&lt;/g, "<");
-      return "<code class='syntax " + lang + "'>" + Koala.render(lang, code) + "</code>";
+      useCode = code;
+      try
+      {
+        useCode = Koala.render(lang, code);
+      }
+      catch(e)
+      {}
+      return "<code class='syntax " + lang + "'>" + useCode + "</code>";
     });
     return text;
   }
